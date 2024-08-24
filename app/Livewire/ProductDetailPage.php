9afeddcbec9e6e6311fa72be01e7a2479769b6c9
Renish437 <renishsiwakoti437@gmail.com>
2024-08-24 17:42:05 +0545

    mains

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,14 +2,44 @@
 
 namespace App\Livewire;
 use App\Models\Product;
+use App\Livewire\Partials\Navbar;
+use App\Helpers\CartManagement;
 use Livewire\Component;
+
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Livewire\Attributes\Title;
 #[Title('Detail Product - GTechMaria')]
 class ProductDetailPage extends Component
 {
+  use LivewireAlert;
     public $slug;
+    public $quantity=1;
     public function mount($slug){
       $this->slug=$slug;
     }
+    public function increaseQty(){
+      $this->quantity++;
+    }
+    public function decreaseQty(){
+      if($this->quantity>1){
+        $this->quantity--;
+      }
+      
+    }
+    
+    public function addToCart($product_id){
+
+      $total_count=CartManagement::addItemToCartWithQty($product_id,$this->quantity);
+
+      $this->dispatch('update-cart-count',total_count: $total_count)->to(Navbar::class);
+
+      $this->alert('success', 'Product added to cart', [
+          'position' => 'bottom-end',
+          'timer' => 3000,
+          'toast' => true,
+         ]);
+
+  }
     public function render()
     {
         return view('livewire.product-detail-page',[
--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -172,7 +172,7 @@
 
     @foreach($brands as $brand)
     <div class="bg-white rounded-lg shadow-md dark:bg-gray-800" wire-key="{{$brand->id }}">
-        <a href="#" class="">
+        <a href="/products?selected_brands[0]={{$brand->id}}" class="">
           <img src="{{url('storage',$brand->image)}}" alt="{{$brand->name}}" class="object-cover w-full h-64 rounded-t-lg">
         </a>
         <div class="p-5 text-center">
@@ -223,7 +223,7 @@
   <div class="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto">
     <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 sm:gap-6">
 @foreach($categories as $category)
-<a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#" wire:key="{{$category->id}}">
+<a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/products?selected_categories[0]={{$category->id}}" wire:key="{{$category->id}}">
         <div class="p-4 md:p-5">
           <div class="flex justify-between items-center">
             <div class="flex items-center">
@@ -450,7 +450,7 @@
             </div>
             <div>
               <h2 class="text-lg font-semibold text-gray-900 dark:text-gray-300">
-                William harry</h2>
+                William Harry</h2>
               <p class="text-xs text-gray-500 dark:text-gray-400">Marketing Officer</p>
             </div>
           </div>
@@ -529,7 +529,7 @@
             </div>
             <div>
               <h2 class="text-lg font-semibold text-gray-900 dark:text-gray-300">
-                James jack</h2>
+                James Jack</h2>
               <p class="text-xs text-gray-500 dark:text-gray-400">Java Programmer</p>
             </div>
           </div>
