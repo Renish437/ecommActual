fdbcb8f4bd75285476d1450e3cad66cfc39e0515
Renish437 <renishsiwakoti437@gmail.com>
2024-08-28 22:34:22 +0545

    mains

--- app/Livewire/Auth/LoginPage.php
+++ app/Livewire/Auth/LoginPage.php
@@ -3,9 +3,28 @@
 namespace App\Livewire\Auth;
 
 use Livewire\Component;
-
+use Livewire\Attributes\Title;
+#[Title('Login')]
 class LoginPage extends Component
 {
+    public $email;
+    public $password;
+
+    public function save(){
+        $this->validate([
+            'email'=>'required|email|max:255',
+            'password'=>'required|min:6|max:255',
+        ]);
+
+        if(!auth()->attempt(['email'=>$this->email,'password'=>$this->password])){
+            session()->flash('error','Invalid Credentials');
+            return;
+           
+        }
+        return redirect()->intended();
+    }
+
+
     public function render()
     {
         return view('livewire.auth.login-page');
